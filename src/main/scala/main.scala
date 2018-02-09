dbfc323a102681e5bcc00a46d8728261e474c653
prasadmate28 <prasad_mate@hotmail.com>
2018-02-08 19:46:22 -0500

    Main.scala commit

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,7 +3,7 @@ package main.scala
 import scalation.columnar_db
 import scalation.random
 import scalation.stat._
-import scalation.analytics.SimpleRegression
+import scalation.analytics.Regression
 import scalation.columnar_db.Relation._
 import scalation.linalgebra.{MatrixD, VectoD, VectorD}
 import scalation.math.double_exp
@@ -13,62 +13,42 @@ import scalation.stat
 
 object main {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-    
-//    val x = new MatrixD ((20, 5), 1.0,   47.0,   85.4,   5.1,    33.0,
-//        1.0,   49.0,   94.2,   3.8,    14.0,
-//        1.0,   49.0,   95.3,   8.2,    10.0,
-//        1.0,   50.0,   94.7,   5.8,    99.0,
-//        1.0,   51.0,   89.4,   7.0,    95.0,
-//        1.0,   48.0,   99.5,   9.3,    10.0,
-//        1.0,   49.0,   99.8,   2.5,    42.0,
-//        1.0,   47.0,   90.9,   6.2,     8.0,
-//        1.0,   49.0,   89.2,   7.1,    62.0,
-//        1.0,   48.0,   92.7,   5.6,    35.0,
-//        1.0,   47.0,   94.4,   5.3,    90.0,
-//        1.0,   49.0,   94.1,   5.6,    21.0,
-//        1.0,   50.0,   91.6,  10.2,    47.0,
-//        1.0,   45.0,   87.1,   5.6,    80.0,
-//        1.0,   52.0,  101.3,  10.0,    98.0,
-//        1.0,   46.0,   94.5,   7.4,    95.0,
-//        1.0,   46.0,   87.0,   3.6,    18.0,
-//        1.0,   46.0,   94.5,   4.3,    12.0,
-//        1.0,   48.0,   90.5,   9.0,    99.0,
-//        1.0,   56.0,   95.7,   7.0,    99.0)
-//    var cor = new MatrixD (5,5)
-    
-    
-//      x(i) = exp.gen + exp.gen+ exp.gen
-//    }
-    
-    val data =  MatrixD("/home/sahar/Downloads/dataset-p1.csv",1)
-    val x = data.selectCols(Array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14))
-    //should add column 1 in the begining!!
-//    val x1 = 1.0 + x.
-//    println("x = "+x)
-//    
-    val y : VectorD = data.col(15)
-    println("y = "+y)
-    
-    val rg = new SimpleRegression (x, y)
-    rg.train ()
+    println("DATA SCIENCE PROJECT ! \n")
+    //val col_index = Array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14)
+    println("+=============== load data ===================")
+    val data = MatrixD("dataset-p1.csv",1)
+    //val x = data.selectCols(Array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14))
 
-    println ("coefficient = " + rg.coefficient)
-    println ("            = " + rg.fitLabels)
-    println ("fit         = " + rg.fit)
-    
-    
-//    var N = 10000
-//    var exp = new scalation.random.Uniform()
-//    val x = new VectorD(N)
-//    
-//    for(i<- 0 until N){
-//      x(i) = exp.gen + exp.gen+ exp.gen
-//    }
-//    
-//    val plot = new Histogram(x,40)
-//      
-//        print(exp.gen)
-    
+    val x = data.sliceCol(0,15)
+    println("rows "+x.dim1+"cols"+x.dim2)
+    //should add column 1 in the begining!!
+    println("+=============== augment 1 vector :: data ===================")
+
+    val x1 = x.+^:(VectorD.one(x.dim1))
+
+    val y : VectorD = data.col(15)
+    println("X1 dimensions "+ x1.dim1 + " "+ x1.dim2)
+    println("y dimentions = "+y.dim)
+    println("+=============== Train using regression ===================")
+
+    val rg = new Regression (x1, y)
+    rg.train()
+    println("========REPORT==============")
+
+    rg.report()
+    //println ("vif      = " + rg.vif)
+
+    // Transformed matrix
+    println("=============== Transforming original matrix ===================")
+
+    val x4sq: VectorD = x1.col(3).map(Math.pow(_,2))
+    println("x4sq dimentions = "+x4sq.dim)
+
+    val data_transformed = data+:x4sq
+    println("X1 dimensions "+ data_transformed.dim1 + " "+ data_transformed.dim2)
+    //print("data_transformed \n"+data_transformed)
+    //val rg_trans = new Regression(data.selectCols(Array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)),y)
+    //rg_trans.train()
+
   }
 }
